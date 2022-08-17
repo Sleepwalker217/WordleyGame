fc1a3dbb1f8e4664f00b904736042e0fb74b616c
Sleepwalker217 <rationrat@yandex.ru>
2022-08-17 23:59:11 +0300

    MVP done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace WordleyGame
 {
@@ -21,43 +22,195 @@ namespace WordleyGame
     public partial class MainWindow : Window
     {
 
+        List<List<Rectangle>> full_recs = new List<List<Rectangle>>(); // двумерный массив прямоугольников
+        List<List<TextBlock>> full_boxes = new List<List<TextBlock>>(); // двумерный массив символов внутри прямоугольников
+        
+        
+        private const int MAX_X = 5;
+        private const int MAX_Y = 6;
 
+        byte indexX = 0;
+        byte indexY = 0;
+        char[] chars = {' ', ' ', ' ', ' ', ' '};
+        string word;
 
-        List<Rectangle> recs = new List<Rectangle>();
-        List<TextBlock> boxes = new List<TextBlock>();
         public MainWindow() // конструктор
         {
             InitializeComponent();
-
-            for(int i=0; i<5; i++)
+            MainGrid.Focus();
+            // создание элементов
+            for (int i = 0; i < MAX_Y; i++)
             {
-                Rectangle curr_Rect = new Rectangle();
-                curr_Rect.Fill = Brushes.Transparent;
-                curr_Rect.Height = 50;
-                curr_Rect.Width = 50;
-                curr_Rect.RadiusX = 10;
-                curr_Rect.RadiusY = 10;
-                curr_Rect.Stroke = Brushes.LightGray;
-                curr_Rect.StrokeThickness = 6;
-                recs.Add(curr_Rect);
-                Grid.SetColumn(recs[i], i);
-                Grid.SetRow(recs[i], 1);
+                List<Rectangle> recs = new List<Rectangle>();
+                List<TextBlock> boxes = new List<TextBlock>();
+                for (int j = 0; j < MAX_X; j++)
+                {
+                    Rectangle curr_Rect = new Rectangle
+                    {
+                        Fill = Brushes.Transparent,
+                        Height = 60,
+                        Width = 60,
+                        RadiusX = 15,
+                        RadiusY = 15,
+                        Stroke = Brushes.LightGray,
+                        StrokeThickness = 6
+                    };
+                    recs.Add(curr_Rect);
+                    Grid.SetColumn(recs[j], j);
+                    Grid.SetRow(recs[j], i + 1);
 
-                btnGrid.Children.Add(recs[i]);
+                    btnGrid.Children.Add(recs[j]);
 
 
-                TextBlock curr_Text = new TextBlock();
-                curr_Text.Text = i.ToString();
-                curr_Text.FontSize = 40;
-                curr_Text.VerticalAlignment = VerticalAlignment.Center;
-                curr_Text.HorizontalAlignment = HorizontalAlignment.Center;
-                curr_Text.Margin = new Thickness(0, 0, 0, 5);
-                curr_Text.FontWeight = FontWeights.Medium;
-                boxes.Add(curr_Text);
-                Grid.SetColumn(boxes[i], i);
-                Grid.SetRow(boxes[i], 1);
-                btnGrid.Children.Add(boxes[i]);
+                    TextBlock curr_Text = new TextBlock
+                    {
+                        FontSize = 40,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        Margin = new Thickness(0, 0, 0, 5),
+                        FontWeight = FontWeights.Medium
+                    };
+                    boxes.Add(curr_Text);
+                    Grid.SetColumn(boxes[j], j);
+                    Grid.SetRow(boxes[j], i + 1);
+                    btnGrid.Children.Add(boxes[j]);
+                }
+                full_recs.Add(recs);
+                full_boxes.Add(boxes);
             }
+
+            /*
+            // создание кнопки с галочкой
+            Button tick = new Button
+            {
+                BorderThickness = new Thickness(0),
+                Background = new ImageBrush(new BitmapImage(new Uri("tick.jpg", UriKind.Relative))),
+                Width = 50,
+                Height = 50,
+                Focusable = false
+            };
+            Grid.SetColumn(tick, 5);
+            Grid.SetRow(tick, 1);
+            btnGrid.Children.Add(tick);
+            */
+
+            Rand_word();
+
+            full_recs[0][0].Stroke = Brushes.Gray;
+
+
+        }
+
+        int Words_length()
+        {
+            StreamReader reader = new StreamReader(@"Words.txt");
+            int res = 0;
+            while (reader.ReadLine() != null) res++;
+            return res;
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    if (indexX != 0) indexX--;
+                    full_recs[indexY][indexX].Stroke = Brushes.Gray;
+                    full_recs[indexY][indexX+1].Stroke = Brushes.LightGray;
+                    break;
+               
+                case Key.Right:
+                    if (indexX != MAX_X-1) indexX++;
+                    full_recs[indexY][indexX].Stroke = Brushes.Gray;
+                    full_recs[indexY][indexX-1].Stroke = Brushes.LightGray;
+                    break;
+                
+                case Key.Enter:
+                    char[] tmp_word = word.ToCharArray();
+                    if (!chars.Contains(' '))
+                    {
+                        if (new string(chars) == word)
+                        {
+                            MessageBox.Show("Congratulations!\nPress OK or ENTER to start a New Game");
+                            Restart();
+                            break;
+                        }
+                        for (int i = 0; i < MAX_X; i++)
+                        {
+                            if (chars[i] == tmp_word[i])
+                                full_recs[indexY][i].Fill = Brushes.LightGreen;
+                            else if (word.Contains(chars[i]))
+                                full_recs[indexY][i].Fill = Brushes.Yellow;
+                            chars[i] = ' ';
+                        }
+                            
+                        
+                        full_recs[indexY][indexX].Stroke = Brushes.LightGray;
+
+                        if (indexY < MAX_Y-1)
+                        {
+                            indexY++;
+                            indexX = 0;
+                            full_recs[indexY][indexX].Stroke = Brushes.Gray;
+                        }
+                        else 
+                        {
+                            string you_lost = string.Format("You lost\nThe word was {0}\nPress OK or ENTER to start a New Game",word); 
+                            MessageBox.Show(you_lost);
+                            Restart();
+                        }
+                        
+
+                    }
+                    break;
+                
+                default:
+                    string temp = e.Key.ToString();
+                    if (temp.Length == 1)
+                    {
+                        full_boxes[indexY][indexX].Text = temp;
+                        chars[indexX] = temp.ToCharArray()[0];
+
+                        if (indexX != MAX_X - 1) indexX++;
+                        full_recs[indexY][indexX].Stroke = Brushes.Gray;
+                        full_recs[indexY][indexX - 1].Stroke = Brushes.LightGray;
+                    }
+
+                    break;
+            }
+
+
+        }
+
+        private void Restart()
+        {
+            indexX = 0;
+            indexY = 0;
+            for (int i = 0; i < MAX_Y; i++)
+            {
+                for (int j = 0; j < MAX_X; j++)
+                {
+                    full_boxes[i][j].Text = " ";
+                    full_recs[i][j].Fill = Brushes.Transparent;
+                    full_recs[i][j].Stroke = Brushes.LightGray;
+                    chars[j] = ' ';
+                    full_recs[indexY][indexX].Stroke = Brushes.Gray;
+                }
+
+                    
+
+            }   
+            Rand_word();
+
+        }
+
+        private void Rand_word()
+        {
+            Random rnd = new Random();
+            StreamReader words = new StreamReader(@"Words.txt");
+            int choose = rnd.Next(0, Words_length() - 1);
+            for (int i = 0; i < choose; i++) words.ReadLine();
+            word = words.ReadLine();
         }
     }
 }
